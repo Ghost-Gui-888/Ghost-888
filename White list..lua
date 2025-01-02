f421e1757bec33851bcb098e5f12ae8086ab948d
Ghost nb <3065395172@qq.com>
2025-01-02 21:28:37 +0800

    Add files via upload

---
+++ White list..lua
@@ -0,0 +1,15 @@
+return {
+["KS_G66"] = true,--鬼
+["CN_BLPO"] = true,--冷
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+}
+
